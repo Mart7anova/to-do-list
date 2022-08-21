c08a444e865b2be74d7f23c0848b773c26a98072
Mart7anova <mart7anova7@gmail.com>
2022-08-21 09:46:03 +0300

    refactor styles

--- src/App/NavigationBar.tsx
+++ src/App/NavigationBar.tsx
@@ -1,30 +1,39 @@
 import React, {useCallback} from 'react';
 import {AppBar, Button, Toolbar} from '@material-ui/core';
 import style from './styles/NavigationBar.module.scss'
-import {AddTodoListFrom} from './AddTodoListFrom';
 import {useAppDispatch} from '../hooks/hooks';
 import {logout} from '../reducers/auth-reducer';
+import {AddItemForm} from '../components/AddItemForm';
+import {createTodoList} from '../reducers/todoList-reducer';
 
 export const NavigationBar = () => {
     const dispatch = useAppDispatch()
 
-    const logoutHandler = useCallback(()=>{
+    const addTodoList = useCallback((title: string) => {
+        dispatch(createTodoList(title))
+    }, [dispatch])
+
+    const logoutHandler = useCallback(() => {
         dispatch(logout())
-    },[dispatch])
+    }, [dispatch])
+
+
 
     return (
-            <AppBar className={style.navigationBar} position="static" color={'default'}>
-                <Toolbar>
-                    <h1 className={style.heading}>TO-DO LIST APP</h1>
-                    <AddTodoListFrom/>
-                    <Button className={style.logoutButton}
-                            variant={'outlined'}
-                            color={'default'}
-                            disableElevation
-                            onClick={logoutHandler}
-                    >Log out
-                    </Button>
-                </Toolbar>
-            </AppBar>
+        <AppBar className={style.AppBar} position={'static'} color={'default'}>
+            <Toolbar className={style.Toolbar}>
+                <h1 className={style.heading}>TO-DO LIST APP</h1>
+                <div className={style.addItemContainer}>
+                    <AddItemForm addItem={addTodoList} itemTitle={'to-do list'}/>
+                </div>
+                <Button className={style.logoutButton}
+                        variant={'outlined'}
+                        color={'default'}
+                        disableElevation
+                        onClick={logoutHandler}
+                >Log out
+                </Button>
+            </Toolbar>
+        </AppBar>
     );
 };
