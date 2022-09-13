5e097a2732a91a1910b0bd6161344cfecaf05089
Mart7anova <mart7anova7@gmail.com>
2022-09-13 13:07:17 +0300

    add redux-toolkit, refactor createStore in configureStore

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,9 +1,10 @@
-import {applyMiddleware, combineReducers, createStore} from 'redux';
+import {combineReducers} from 'redux';
 import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk';
 import {todoListActionType, todoListReducer} from '../reducers/todoList-reducer';
 import {TaskActionType, taskReducer} from '../reducers/task-reducer';
 import {AppActionType, appReducer} from '../reducers/app-reducer';
 import {AuthActionType, authReducer} from '../reducers/auth-reducer';
+import {configureStore} from '@reduxjs/toolkit'
 
 const rootReducer = combineReducers({
     app:appReducer,
@@ -12,7 +13,10 @@ const rootReducer = combineReducers({
     task: taskReducer
 })
 
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware))
+export const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware)
+});
 
 export type RootSReducerType = typeof rootReducer
 export type RootStateType = ReturnType<RootSReducerType>
