ecd21cc14d2316ec93a703a97354c9e5f611f34e
Mart7anova <mart7anova7@gmail.com>
2022-09-14 15:23:21 +0300

    refactor types in store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,28 +1,20 @@
-import {AnyAction, combineReducers} from 'redux';
-import thunkMiddleware, {ThunkAction, ThunkDispatch} from 'redux-thunk';
-import {todoListReducer} from '../reducers/todoList-reducer';
-import {taskReducer} from '../reducers/task-reducer';
-import {appReducer} from '../reducers/app-reducer';
-import {authReducer} from '../reducers/auth-reducer';
+import {todoListReducer} from './reducers/todoList-reducer';
+import {taskReducer} from './reducers/task-reducer';
+import {appReducer} from './reducers/app-reducer';
+import {authReducer} from './reducers/auth-reducer';
 import {configureStore} from '@reduxjs/toolkit'
 
-const rootReducer = combineReducers({
-    app:appReducer,
-    auth: authReducer,
-    todoList: todoListReducer,
-    task: taskReducer
-})
-
 export const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().prepend(thunkMiddleware)
+    reducer: {
+        app:appReducer,
+        auth: authReducer,
+        todoList: todoListReducer,
+        task: taskReducer
+    },
 });
 
-export type RootSReducerType = typeof rootReducer
-export type RootStateType = ReturnType<RootSReducerType>
-
-export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, AnyAction>
-export type AppDispatch = ThunkDispatch<RootStateType, unknown, AnyAction>
+export type RootStateType = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
 
 
 // @ts-ignore
